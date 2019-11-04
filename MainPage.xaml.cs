875d9deb76679d333153e54615174a3503af7e39
svevash <44944036+svevash@users.noreply.github.com>
2019-11-04 23:25:22 +1000

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace testmobil
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var page1 = new Page1();
+            
+            page1.Disappearing += (a, b) => {
+                if (page1.text != null)
+                {
+                    Label label = new Label();
+                    label.Text = page1.text.Split('\n')[0];
+                    label.BackgroundColor = Color.LightCoral;
+                    label.FontSize = 30;
+
+                    var tapG = new TapGestureRecognizer();
+                    tapG.Tapped += (tapsender, taps) =>
+                    {
+
+                        //label.Text = "gavno";
+                        var editor = new Page1(page1.text);
+                        editor.Disappearing += (c, d) => {
+                            page1.text = editor.text;
+
+                            label.Text = editor.text.Split('\n')[0];
+                        };
+                        Navigation.PushAsync(editor);
+                    };
+                    label.GestureRecognizers.Add(tapG);
+                    stacklayout1.Children.Add(label);
+                }
+            };
+            Navigation.PushAsync(page1);
+        }
+    }
+}
---
+++ Page1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace testmobil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public string text = null;
+        public Page1()
+        {
+            InitializeComponent();
+            text = null;
+        }
+
+        public Page1(string s)
+        {
+            InitializeComponent();
+            TextHolder.Text = s;
+            text = s;
+        }
+
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            text = TextHolder.Text;
+            Navigation.PopAsync();
+        }
+    }
+
+    
+}
