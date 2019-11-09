54f3b9e428a7f7fd5228d9de500470ef71c4ccc6
svevash <44944036+svevash@users.noreply.github.com>
2019-11-09 14:31:53 +1000

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -25,26 +25,56 @@ namespace testmobil
             page1.Disappearing += (a, b) => {
                 if (page1.text != null)
                 {
+                    Frame frame = new Frame
+                    {
+                        BorderColor = Color.Black
+                    };
+
+                    frame.BackgroundColor = Color.LightCoral;
+
                     Label label = new Label();
                     label.Text = page1.text.Split('\n')[0];
                     label.BackgroundColor = Color.LightCoral;
                     label.FontSize = 30;
+                    frame.Content = label;
 
                     var tapG = new TapGestureRecognizer();
                     tapG.Tapped += (tapsender, taps) =>
                     {
-
-                        //label.Text = "gavno";
                         var editor = new Page1(page1.text);
                         editor.Disappearing += (c, d) => {
                             page1.text = editor.text;
-
                             label.Text = editor.text.Split('\n')[0];
                         };
                         Navigation.PushAsync(editor);
                     };
+                    
                     label.GestureRecognizers.Add(tapG);
-                    stacklayout1.Children.Add(label);
+
+                    var swipe = new SwipeGestureRecognizer();
+
+                    if (stacklayout1.Height > stacklayout2.Height)
+                    {
+                        
+                        swipe.Direction = SwipeDirection.Right;
+                        swipe.Swiped += (swipesender, swipes) =>
+                        {
+                            stacklayout1.Children.Remove(frame);
+                        };
+                        stacklayout2.Children.Add(frame);
+                    }
+                    else
+                    {
+                        
+                        swipe.Direction = SwipeDirection.Right;
+                        swipe.Swiped += (swipesender, swipes) =>
+                        {
+                            stacklayout2.Children.Remove(frame);
+                        };
+                        stacklayout1.Children.Add(frame);
+                    }
+                    frame.GestureRecognizers.Add(swipe);
+
                 }
             };
             Navigation.PushAsync(page1);
